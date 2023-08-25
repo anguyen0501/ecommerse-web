f7f9b70f5d2440224035a9e7726bae12da9f4485
Dat Tien Le <letiendat1002@gmail.com>
2023-08-25 15:12:18 +0700

    add database sql

---
+++ assets/database.sql
@@ -0,0 +1,31 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`AccountUser`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`AccountUser` (
+  `Email` VARCHAR(255) NOT NULL,
+  `Password` VARCHAR(255) NOT NULL,
+  `FullName` VARCHAR(255) NOT NULL,
+  `Gender` VARCHAR(255) NOT NULL,
+  `Nationality` VARCHAR(255) NOT NULL,
+  `Note` LONGTEXT NULL,
+  PRIMARY KEY (`Email`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
